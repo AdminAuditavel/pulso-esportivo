6c004f090f2f54630895e9e302ddecaaa3ac9041
AdminAuditavel <admin@auditavel.com>
2025-12-23 20:43:58 -0300

    Create index.js

---
+++ frontend/pages/index.js
@@ -0,0 +1,9 @@
+// frontend/pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Bem-vindo à plataforma Pulso!</h1>
+      <p>Esta é a página inicial.</p>
+    </div>
+  );
+}
